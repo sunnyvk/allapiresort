7c5ce2707ca8e163c20007293725bd6b3a37f2f1
sunnyvk <khambayatsunny278@gmail.com>
2023-03-30 23:17:52 +0530

    first

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
+app.use(express.json());
 app.use(cookieParser());
 
 
